23a1cd209a27b4f6c8b26f47466a21833e689344
Adalberto Cerrillo <34152734@uaz.edu.mx>
2024-02-09 20:53:46 -0600

    Pruebas unitarias

---
+++ pruebas_doctest/pruebas_unitarias/edades/test_edades.py
@@ -0,0 +1,80 @@
+from edades import edades
+import unittest
+
+class Test_promedio(unittest.TestCase):
+    def setUp(self):
+        self.e = edades()
+
+    def tearDown(self):
+        pass
+
+    def test_no_existes(self):
+        resultado = self.e.mensaje(-5)
+        self.assertEqual('No existes', resultado)
+
+    def test_no_existes_2(self):
+        resultado = self.e.mensaje(-1)
+        self.assertEqual('No existes', resultado)
+
+    def test_eres_ninio(self):
+        resultado = self.e.mensaje(0)
+        self.assertEqual('Eres niño', resultado)
+
+    def test_eres_ninio_2(self):
+        resultado = self.e.mensaje(5)
+        self.assertEqual('Eres niño', resultado)
+
+    def test_eres_adolescente(self):
+        resultado = self.e.mensaje(13)
+        self.assertEqual('Eres adolescente', resultado)
+
+    def test_eres_adolescente_2(self):
+        resultado = self.e.mensaje(17)
+        self.assertEqual('Eres adolescente', resultado)
+
+    def test_eres_Adulto(self):
+        resultado = self.e.mensaje(30)
+        self.assertEqual('Eres adulto', resultado)
+
+    def test_eres_Adulto_2(self):
+        resultado = self.e.mensaje(64)
+        self.assertEqual('Eres adulto', resultado)
+
+    def test_Eres_adulto_mayor(self):
+        resultado = self.e.mensaje(119)
+        self.assertEqual('Eres adulto mayor', resultado)
+
+    def test_Eres_adulto_mayor_2(self):
+        resultado = self.e.mensaje(80)
+        self.assertEqual('Eres adulto mayor', resultado)
+
+    def test_mumm_ra(self):
+        resultado = self.e.mensaje(150)
+        self.assertEqual('Eres Mumm-Ra', resultado)
+
+    def test_mumm_ra_2(self):
+        resultado = self.e.mensaje(1000)
+        self.assertEqual('Eres Mumm-Ra', resultado)
+
+    def test_edad_cadena(self):
+        resultado = self.e.mensaje('hola')
+        self.assertEqual('No se permiten cadenas', resultado)
+
+    def test_edad_decimal(self):
+        resultado = self.e.mensaje(12.4)
+        self.assertEqual('Solo numeros enteros', resultado)
+
+    def test_edad_true(self):
+        resultado = self.e.mensaje(True)
+        self.assertEqual('No se permiten booleanos', resultado)
+
+    def test_edad_lista(self):
+        resultado = self.e.mensaje([])
+        self.assertEqual('No se permiten listas', resultado)
+        
+    def test_edad_diccionario(self):
+        resultado = self.e.mensaje({})
+        self.assertEqual('Elemento proporcionado no válido', resultado)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ pruebas_doctest/pruebas_unitarias/promedio/test_promedio.py
@@ -0,0 +1,52 @@
+from promedio import promedio
+import unittest
+
+class Test_promedio(unittest.TestCase):
+    def setUp(self):
+        self.p = promedio()
+
+    def tearDown(self):
+        pass
+
+    def test_promedio_enteros(self):
+        resultado = self.p.calcular(10,10,10)
+        self.assertEqual(10.0, resultado)
+
+    def test_promedio_decimales(self):
+        resultado = self.p.calcular(9,9.5,7)
+        self.assertEqual(8.5, resultado)
+
+    def test_promedio_enteros_2(self):
+        resultado = self.p.calcular(5,6,6)
+        self.assertEqual(5.67, resultado)
+
+    def test_promedio_cadenas(self):
+        resultado = self.p.calcular('a',10,10)
+        self.assertEqual('No se aceptan cadenas de texto', resultado)
+
+    def test_promedio_listas(self):
+        resultado = self.p.calcular([],10,10)
+        self.assertEqual('No se aceptan listas', resultado)
+
+    def test_promedio_booleanos(self):
+        resultado = self.p.calcular(10,True,10)
+        self.assertEqual('No se aceptan booleanos', resultado)
+
+    def test_promedios_negativos(self):
+        resultado = self.p.calcular(-5,10,10)
+        self.assertEqual('No se aceptan numeros negativos', resultado)
+
+    def test_promedios_mayores_limite(self):
+        resultado = self.p.calcular(10,20,10)
+        self.assertEqual('La calificación máxima es 10', resultado)
+
+    def test_promedios_elemento_no_valido(self):
+        resultado = self.p.calcular(10,{},10)
+        self.assertEqual('Elementos no válidos para el cálculo', resultado)
+
+    def test_promedios_nulos(self):
+        resultado = self.p.calcular(10,None,10)
+        self.assertEqual('No se aceptan valores nulos', resultado)
+
+if __name__ == '__main__':
+    unittest.main()
